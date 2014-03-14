5435199ffe7e1fe3caaa26da634083067e6b8cc5
JoeSimsen <joeysimsen@gmail.com>
2014-03-14 13:28:55 +0100

    Websitenaam goedgezet

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Improvements',
+	'name' => 'Joey Simsen',
 
 	// preloading 'log' component
 	'preload'=>array('log','bootstrap'),
